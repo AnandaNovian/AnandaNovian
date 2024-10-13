cb31c678558c4b35b7073c062ccfddca5258d4cd
AnandaNovian <novianananda75@gmail.com>
2024-10-13 19:06:53 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inputkan angka: ");
+        int n = input.nextInt();
+        if(n % 2 == 1){
+            System.out.println("Ganjil");
+        }else{
+            System.out.println("Genap");
+        }
+        input.close();
+    }
+}
