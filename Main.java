87a5aad0744bdcce9b653b261bc83db9ed58624f
AnandaNovian <novianananda75@gmail.com>
2024-10-14 09:01:07 +0700

    GanjilGenap.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main{
+public class     GanjilGenap{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Inputkan angka: ");
